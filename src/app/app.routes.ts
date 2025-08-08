4a88a954028d3ee19445d5469ef876ec9274e4cb
ZunainAliAzam <zunain.azam@iomechs.com>
2025-08-08 17:22:17 +0500

    feat/added routes and angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './calyndra-pages/home-page/home-page';
 import { Login } from './calyndra-pages/login/login';
+import { Layout } from './layouts/layout/layout';
+import { ProfilePage } from './calyndra-pages/profile-page/profile-page';
+import { Dashboard } from './calyndra-pages/dashboard/dashboard';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'login', component: Login },
   { path: 'signup', component: Login },
+  { 
+    path: 'org/home', 
+    component: Layout,
+    children: [
+      { path: '', component: Dashboard },
+      { path: 'dashboard', component: Dashboard }
+    ]
+  },
+  { path: 'profile', component: ProfilePage },
   { path: '**', redirectTo: '' },
 ];
--- src/app/calyndra-pages/login/login.ts
+++ src/app/calyndra-pages/login/login.ts
@@ -1,34 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
   imports: [CommonModule, FormsModule],
   selector: 'app-login',
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
 export class Login {
   isLoginMode = true;
-  
+
   // Password visibility toggles
-  showLoginPassword = false;
-  showSignupPassword = false;
-  showConfirmPassword = false;
-  
+  showLoginPassword: boolean = false;
+  showSignupPassword: boolean = false;
+  showConfirmPassword: boolean = false;
+
+  constructor(private router: Router) {}
   // Login form data
   loginData = {
     username: '',
-    password: ''
+    password: '',
   };
-  
+
   // Signup form data
   signupData = {
     username: '',
     email: '',
     password: '',
-    confirmPassword: ''
+    confirmPassword: '',
   };
 
   toggleMode() {
@@ -54,6 +56,7 @@ export class Login {
   onSubmit() {
     if (this.isLoginMode) {
       console.log('Login submitted:', this.loginData);
+      this.router.navigate(['/org/home']);
       // Handle login logic here
     } else {
       console.log('Signup submitted:', this.signupData);
