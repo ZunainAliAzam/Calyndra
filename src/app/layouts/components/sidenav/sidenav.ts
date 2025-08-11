88a258d47076554709ad131aa60bb6595c11278e
ZunainAliAzam <zunain.azam@iomechs.com>
2025-08-11 18:23:56 +0500

    feat/added new routes and navigation for admin panel

--- src/app/layouts/components/sidenav/sidenav.ts
+++ src/app/layouts/components/sidenav/sidenav.ts
@@ -2,9 +2,11 @@ import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { SidebarService } from '../../../core/services/sidebar.service';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
+  standalone: true,
   imports: [CommonModule, MatIconModule],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.css'
@@ -12,6 +14,7 @@ import { CommonModule } from '@angular/common';
 export class Sidenav {
   private sidebarService = inject(SidebarService);
   
+  constructor(private router: Router) {}
   // Expose signals for template binding
   sidebarMode = this.sidebarService.sidebarMode;
   isOpen = this.sidebarService.isOpen;
@@ -21,9 +24,7 @@ export class Sidenav {
     this.sidebarService.toggle();
   }
   
-  onNavigationClick(): void {
-    if (this.sidebarMode() === 'over') {
-      this.sidebarService.close();
-    }
+  onNavigationClick(link: string): void {
+    this.router.navigate(['/org', link]);
   }
 }
